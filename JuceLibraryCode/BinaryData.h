b45117ff71a5483b452aa80df191d5233d023594
Larry Seyer <larryseyer@users.noreply.github.com>
2025-07-24 11:51:53 -0500

    after projucer init

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,555 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   LICENSE_txt;
+    const int            LICENSE_txtSize = 1070;
+
+    extern const char*   Acoustic_Dry_sfz;
+    const int            Acoustic_Dry_sfzSize = 101;
+
+    extern const char*   Acoustic_Room_sfz;
+    const int            Acoustic_Room_sfzSize = 101;
+
+    extern const char*   Acoustic_Wet_sfz;
+    const int            Acoustic_Wet_sfzSize = 101;
+
+    extern const char*   Acoustic_sfz;
+    const int            Acoustic_sfzSize = 101;
+
+    extern const char*   Hat_wav;
+    const int            Hat_wavSize = 457256;
+
+    extern const char*   hihat_wav;
+    const int            hihat_wavSize = 457256;
+
+    extern const char*   Kick_wav;
+    const int            Kick_wavSize = 184458;
+
+    extern const char*   Snare_wav;
+    const int            Snare_wavSize = 383258;
+
+    extern const char*   Electronic_sfz;
+    const int            Electronic_sfzSize = 101;
+
+    extern const char*   Hat_wav2;
+    const int            Hat_wav2Size = 457256;
+
+    extern const char*   hihat_wav2;
+    const int            hihat_wav2Size = 457256;
+
+    extern const char*   Kick_wav2;
+    const int            Kick_wav2Size = 184458;
+
+    extern const char*   Snare_wav2;
+    const int            Snare_wav2Size = 383258;
+
+    extern const char*   Hat_wav3;
+    const int            Hat_wav3Size = 457256;
+
+    extern const char*   hihat_wav3;
+    const int            hihat_wav3Size = 457256;
+
+    extern const char*   Kick_wav3;
+    const int            Kick_wav3Size = 184458;
+
+    extern const char*   Percussion_Bongos_sfz;
+    const int            Percussion_Bongos_sfzSize = 101;
+
+    extern const char*   Percussion_Claps_sfz;
+    const int            Percussion_Claps_sfzSize = 101;
+
+    extern const char*   Percussion_Claves_sfz;
+    const int            Percussion_Claves_sfzSize = 101;
+
+    extern const char*   Percussion_Congas_sfz;
+    const int            Percussion_Congas_sfzSize = 101;
+
+    extern const char*   Percussion_Shakers_sfz;
+    const int            Percussion_Shakers_sfzSize = 101;
+
+    extern const char*   Percussion_sfz;
+    const int            Percussion_sfzSize = 101;
+
+    extern const char*   Snare_wav3;
+    const int            Snare_wav3Size = 383258;
+
+    extern const char*   MontserratItalicVariableFont_wght_ttf;
+    const int            MontserratItalicVariableFont_wght_ttfSize = 701156;
+
+    extern const char*   MontserratVariableFont_wght_ttf;
+    const int            MontserratVariableFont_wght_ttfSize = 688600;
+
+    extern const char*   OpenSansItalicVariableFont_wdthwght_ttf;
+    const int            OpenSansItalicVariableFont_wdthwght_ttfSize = 580280;
+
+    extern const char*   OpenSansVariableFont_wdthwght_ttf;
+    const int            OpenSansVariableFont_wdthwght_ttfSize = 529700;
+
+    extern const char*   OrbitronVariableFont_wght_ttf;
+    const int            OrbitronVariableFont_wght_ttfSize = 38548;
+
+    extern const char*   PhosphorBold_ttf;
+    const int            PhosphorBold_ttfSize = 495308;
+
+    extern const char*   PhosphorDuotone_ttf;
+    const int            PhosphorDuotone_ttfSize = 567808;
+
+    extern const char*   PhosphorFill_ttf;
+    const int            PhosphorFill_ttfSize = 449252;
+
+    extern const char*   PhosphorLight_ttf;
+    const int            PhosphorLight_ttfSize = 536628;
+
+    extern const char*   Phosphor_ttf;
+    const int            Phosphor_ttfSize = 488636;
+
+    extern const char*   PhosphorThin_ttf;
+    const int            PhosphorThin_ttfSize = 535152;
+
+    extern const char*   PlayfairDisplayItalicVariableFont_wght_ttf;
+    const int            PlayfairDisplayItalicVariableFont_wght_ttfSize = 280660;
+
+    extern const char*   PlayfairDisplayVariableFont_wght_ttf;
+    const int            PlayfairDisplayVariableFont_wght_ttfSize = 301576;
+
+    extern const char*   RobotoItalicVariableFont_wdthwght_ttf;
+    const int            RobotoItalicVariableFont_wdthwght_ttfSize = 497124;
+
+    extern const char*   RobotoVariableFont_wdthwght_ttf;
+    const int            RobotoVariableFont_wdthwght_ttfSize = 468308;
+
+    extern const char*   RobotoCondensedItalicVariableFont_wght_ttf;
+    const int            RobotoCondensedItalicVariableFont_wght_ttfSize = 387312;
+
+    extern const char*   RobotoCondensedVariableFont_wght_ttf;
+    const int            RobotoCondensedVariableFont_wght_ttfSize = 370940;
+
+    extern const char*   Button080_png;
+    const int            Button080_pngSize = 18727;
+
+    extern const char*   Button100_png;
+    const int            Button100_pngSize = 22136;
+
+    extern const char*   OTTO_GLYPH_png;
+    const int            OTTO_GLYPH_pngSize = 1910987;
+
+    extern const char*   OTTO_Icon_png;
+    const int            OTTO_Icon_pngSize = 856697;
+
+    extern const char*   OTTO_Splash_Screen_png;
+    const int            OTTO_Splash_Screen_pngSize = 89866;
+
+    extern const char*   OTTO_Button_Glyph_png;
+    const int            OTTO_Button_Glyph_pngSize = 38746;
+
+    extern const char*   Slider100_png;
+    const int            Slider100_pngSize = 125269;
+
+    extern const char*   SliderKnob_png;
+    const int            SliderKnob_pngSize = 2193;
+
+    extern const char*   _70s_Rock_Fill_01_MID;
+    const int            _70s_Rock_Fill_01_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_02_MID;
+    const int            _70s_Rock_Fill_02_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_03_MID;
+    const int            _70s_Rock_Fill_03_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_04_MID;
+    const int            _70s_Rock_Fill_04_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_05_MID;
+    const int            _70s_Rock_Fill_05_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_06_MID;
+    const int            _70s_Rock_Fill_06_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_07_MID;
+    const int            _70s_Rock_Fill_07_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_08_MID;
+    const int            _70s_Rock_Fill_08_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_09_MID;
+    const int            _70s_Rock_Fill_09_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_10_MID;
+    const int            _70s_Rock_Fill_10_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_11_MID;
+    const int            _70s_Rock_Fill_11_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_12_MID;
+    const int            _70s_Rock_Fill_12_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_13_MID;
+    const int            _70s_Rock_Fill_13_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_14_MID;
+    const int            _70s_Rock_Fill_14_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_15_MID;
+    const int            _70s_Rock_Fill_15_MIDSize = 265;
+
+    extern const char*   _70s_Rock_Fill_16_MID;
+    const int            _70s_Rock_Fill_16_MIDSize = 265;
+
+    extern const char*   _80s_Brit_Rock_Fill_01_MID;
+    const int            _80s_Brit_Rock_Fill_01_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_02_MID;
+    const int            _80s_Brit_Rock_Fill_02_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_03_MID;
+    const int            _80s_Brit_Rock_Fill_03_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_04_MID;
+    const int            _80s_Brit_Rock_Fill_04_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_05_MID;
+    const int            _80s_Brit_Rock_Fill_05_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_06_MID;
+    const int            _80s_Brit_Rock_Fill_06_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_07_MID;
+    const int            _80s_Brit_Rock_Fill_07_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_08_MID;
+    const int            _80s_Brit_Rock_Fill_08_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_09_MID;
+    const int            _80s_Brit_Rock_Fill_09_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_10_MID;
+    const int            _80s_Brit_Rock_Fill_10_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_11_MID;
+    const int            _80s_Brit_Rock_Fill_11_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_12_MID;
+    const int            _80s_Brit_Rock_Fill_12_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_13_MID;
+    const int            _80s_Brit_Rock_Fill_13_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_14_MID;
+    const int            _80s_Brit_Rock_Fill_14_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_15_MID;
+    const int            _80s_Brit_Rock_Fill_15_MIDSize = 257;
+
+    extern const char*   _80s_Brit_Rock_Fill_16_MID;
+    const int            _80s_Brit_Rock_Fill_16_MIDSize = 257;
+
+    extern const char*   go_sh;
+    const int            go_shSize = 563;
+
+    extern const char*   _70s_Rock_MID;
+    const int            _70s_Rock_MIDSize = 265;
+
+    extern const char*   _80s_Brit_Rock_MID;
+    const int            _80s_Brit_Rock_MIDSize = 257;
+
+    extern const char*   _80s_Metal_MID;
+    const int            _80s_Metal_MIDSize = 222;
+
+    extern const char*   _80s_Rock_MID;
+    const int            _80s_Rock_MIDSize = 213;
+
+    extern const char*   _90s_Alternative_MID;
+    const int            _90s_Alternative_MIDSize = 346;
+
+    extern const char*   _90s_Alternative2_MID;
+    const int            _90s_Alternative2_MIDSize = 258;
+
+    extern const char*   _90s_Indie_Rock_MID;
+    const int            _90s_Indie_Rock_MIDSize = 291;
+
+    extern const char*   Add_an_Organ_MID;
+    const int            Add_an_Organ_MIDSize = 350;
+
+    extern const char*   Afro_Cuban_Pop_MID;
+    const int            Afro_Cuban_Pop_MIDSize = 369;
+
+    extern const char*   Afro_Fusion_MID;
+    const int            Afro_Fusion_MIDSize = 414;
+
+    extern const char*   Aint_it_Sad_Country_MID;
+    const int            Aint_it_Sad_Country_MIDSize = 271;
+
+    extern const char*   Alt_Country_MID;
+    const int            Alt_Country_MIDSize = 382;
+
+    extern const char*   Alt_Rock_MID;
+    const int            Alt_Rock_MIDSize = 320;
+
+    extern const char*   Bad_News_Country_MID;
+    const int            Bad_News_Country_MIDSize = 283;
+
+    extern const char*   Badu_Beat_MID;
+    const int            Badu_Beat_MIDSize = 321;
+
+    extern const char*   Basic_House_MID;
+    const int            Basic_House_MIDSize = 280;
+
+    extern const char*   Basic_Reggae_MID;
+    const int            Basic_Reggae_MIDSize = 255;
+
+    extern const char*   Basic_Swing_MID;
+    const int            Basic_Swing_MIDSize = 293;
+
+    extern const char*   Basic_mid;
+    const int            Basic_midSize = 857;
+
+    extern const char*   Big_Funk_MID;
+    const int            Big_Funk_MIDSize = 281;
+
+    extern const char*   Boogie_Disco_MID;
+    const int            Boogie_Disco_MIDSize = 252;
+
+    extern const char*   Boogie_Woogie_MID;
+    const int            Boogie_Woogie_MIDSize = 258;
+
+    extern const char*   Bossa_Fusion_MID;
+    const int            Bossa_Fusion_MIDSize = 348;
+
+    extern const char*   Bossa_Straight_MID;
+    const int            Bossa_Straight_MIDSize = 271;
+
+    extern const char*   Brazilian_Ballad_MID;
+    const int            Brazilian_Ballad_MIDSize = 251;
+
+    extern const char*   Brazilian_Carnival_MID;
+    const int            Brazilian_Carnival_MIDSize = 420;
+
+    extern const char*   British_Ballad_MID;
+    const int            British_Ballad_MIDSize = 269;
+
+    extern const char*   Busy_Bossa_MID;
+    const int            Busy_Bossa_MIDSize = 462;
+
+    extern const char*   Cha_Cha_MID;
+    const int            Cha_Cha_MIDSize = 283;
+
+    extern const char*   Dancehall_MID;
+    const int            Dancehall_MIDSize = 282;
+
+    extern const char*   DC_Funk_Punk_MID;
+    const int            DC_Funk_Punk_MIDSize = 422;
+
+    extern const char*   Disco_MID;
+    const int            Disco_MIDSize = 293;
+
+    extern const char*   Driving_Three_Four_MID;
+    const int            Driving_Three_Four_MIDSize = 235;
+
+    extern const char*   Dynamite_Funk_MID;
+    const int            Dynamite_Funk_MIDSize = 247;
+
+    extern const char*   East_Coast_Rap_MID;
+    const int            East_Coast_Rap_MIDSize = 287;
+
+    extern const char*   Electro_Jazz_MID;
+    const int            Electro_Jazz_MIDSize = 409;
+
+    extern const char*   Faster_Fusion_MID;
+    const int            Faster_Fusion_MIDSize = 373;
+
+    extern const char*   Four_on_Floor_Rock_MID;
+    const int            Four_on_Floor_Rock_MIDSize = 292;
+
+    extern const char*   French_Carribean_MID;
+    const int            French_Carribean_MIDSize = 320;
+
+    extern const char*   Funky_Drummer_MID;
+    const int            Funky_Drummer_MIDSize = 314;
+
+    extern const char*   Funky_Shuffle_MID;
+    const int            Funky_Shuffle_MIDSize = 385;
+
+    extern const char*   Fusion_Funk_MID;
+    const int            Fusion_Funk_MIDSize = 568;
+
+    extern const char*   Get_on_the_Floor_Funk_MID;
+    const int            Get_on_the_Floor_Funk_MIDSize = 285;
+
+    extern const char*   Grand_Funk_MID;
+    const int            Grand_Funk_MIDSize = 262;
+
+    extern const char*   Half_Time_Rims_MID;
+    const int            Half_Time_Rims_MIDSize = 373;
+
+    extern const char*   Hard_Funk_MID;
+    const int            Hard_Funk_MIDSize = 320;
+
+    extern const char*   Hard_Rock_MID;
+    const int            Hard_Rock_MIDSize = 271;
+
+    extern const char*   Heavy_Funk_MID;
+    const int            Heavy_Funk_MIDSize = 329;
+
+    extern const char*   Heavy_Tom_Groove_MID;
+    const int            Heavy_Tom_Groove_MIDSize = 360;
+
+    extern const char*   Hip_Hop_Funk_MID;
+    const int            Hip_Hop_Funk_MIDSize = 463;
+
+    extern const char*   In_A_Hurry_Funk_MID;
+    const int            In_A_Hurry_Funk_MIDSize = 299;
+
+    extern const char*   Jazz_Fusion_MID;
+    const int            Jazz_Fusion_MIDSize = 349;
+
+    extern const char*   Latin_Funk_MID;
+    const int            Latin_Funk_MIDSize = 267;
+
+    extern const char*   Minimal_Hip_Hop_MID;
+    const int            Minimal_Hip_Hop_MIDSize = 255;
+
+    extern const char*   New_Indie_Rock_MID;
+    const int            New_Indie_Rock_MIDSize = 354;
+
+    extern const char*   New_Metal_MID;
+    const int            New_Metal_MIDSize = 229;
+
+    extern const char*   New_School_Soul_MID;
+    const int            New_School_Soul_MIDSize = 344;
+
+    extern const char*   New_Wave_MID;
+    const int            New_Wave_MIDSize = 416;
+
+    extern const char*   Nice_Slacks_Retro_Rock_MID;
+    const int            Nice_Slacks_Retro_Rock_MIDSize = 278;
+
+    extern const char*   Night_Moves_MID;
+    const int            Night_Moves_MIDSize = 327;
+
+    extern const char*   Old_School_Hip_Hop_MID;
+    const int            Old_School_Hip_Hop_MIDSize = 319;
+
+    extern const char*   Rhumba_MID;
+    const int            Rhumba_MIDSize = 358;
+
+    extern const char*   Rimshot_Ballad_MID;
+    const int            Rimshot_Ballad_MIDSize = 248;
+
+    extern const char*   Rio_Dance_Floor_MID;
+    const int            Rio_Dance_Floor_MIDSize = 318;
+
+    extern const char*   Rockin_Country_MID;
+    const int            Rockin_Country_MIDSize = 317;
+
+    extern const char*   Rolling_Funk_MID;
+    const int            Rolling_Funk_MIDSize = 356;
+
+    extern const char*   RudeBoy_Break_MID;
+    const int            RudeBoy_Break_MIDSize = 418;
+
+    extern const char*   Samba_Swing_MID;
+    const int            Samba_Swing_MIDSize = 444;
+
+    extern const char*   Serious_Funk_MID;
+    const int            Serious_Funk_MIDSize = 315;
+
+    extern const char*   Shuffle_Funk_MID;
+    const int            Shuffle_Funk_MIDSize = 322;
+
+    extern const char*   Shuffle_MID;
+    const int            Shuffle_MIDSize = 277;
+
+    extern const char*   Simple_Reggae_MID;
+    const int            Simple_Reggae_MIDSize = 214;
+
+    extern const char*   Smart_Rock_MID;
+    const int            Smart_Rock_MIDSize = 317;
+
+    extern const char*   Smooth_Funk_MID;
+    const int            Smooth_Funk_MIDSize = 349;
+
+    extern const char*   Soft_and_Lovely_Funk_MID;
+    const int            Soft_and_Lovely_Funk_MIDSize = 264;
+
+    extern const char*   Straight_Rock_MID;
+    const int            Straight_Rock_MIDSize = 259;
+
+    extern const char*   Surfer_Rock_MID;
+    const int            Surfer_Rock_MIDSize = 280;
+
+    extern const char*   Swingin_Funk_MID;
+    const int            Swingin_Funk_MIDSize = 331;
+
+    extern const char*   Syncopated_Fusion_MID;
+    const int            Syncopated_Fusion_MIDSize = 373;
+
+    extern const char*   Texas_Blues_Jam_MID;
+    const int            Texas_Blues_Jam_MIDSize = 303;
+
+    extern const char*   Texas_Rock_MID;
+    const int            Texas_Rock_MIDSize = 335;
+
+    extern const char*   Three_Four_Ballad_MID;
+    const int            Three_Four_Ballad_MIDSize = 236;
+
+    extern const char*   Three_Four_Blues_MID;
+    const int            Three_Four_Blues_MIDSize = 297;
+
+    extern const char*   Three_Four_Rock_MID;
+    const int            Three_Four_Rock_MIDSize = 330;
+
+    extern const char*   Trip_Hop_doesnt_exist_MID;
+    const int            Trip_Hop_doesnt_exist_MIDSize = 451;
+
+    extern const char*   UK_Reggae_MID;
+    const int            UK_Reggae_MIDSize = 259;
+
+    extern const char*   Upbeat_Rock_MID;
+    const int            Upbeat_Rock_MIDSize = 250;
+
+    extern const char*   Urban_Funk_MID;
+    const int            Urban_Funk_MIDSize = 322;
+
+    extern const char*   Verse_MID;
+    const int            Verse_MIDSize = 257;
+
+    extern const char*   Verse1_MID;
+    const int            Verse1_MIDSize = 385;
+
+    extern const char*   Verse2_MID;
+    const int            Verse2_MIDSize = 368;
+
+    extern const char*   West_Coast_Groove_MID;
+    const int            West_Coast_Groove_MIDSize = 280;
+
+    extern const char*   With_Musical_Guest_Funk_MID;
+    const int            With_Musical_Guest_Funk_MIDSize = 366;
+
+    extern const char*   Zydeco_MID;
+    const int            Zydeco_MIDSize = 438;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 176;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
